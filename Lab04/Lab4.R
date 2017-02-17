b605f54d2e12f4c12a1c50843b06210310c50099
JohnRosol <rosol.john@gmail.com>
2017-02-16 17:44:49 -0700

    lab 04 r file final updated

--- Lab04/Lab4.R
+++ Lab04/Lab4.R
@@ -4,7 +4,7 @@ for ( i in 1:10) {
   print("hi")
 }
 
-#lab step 2
+#lab step 2, using calculations in a vector
 allowance <- 5
 gum <- (2*1.34)
 piggybank <- 10
@@ -14,7 +14,7 @@ for ( j in seq(1,8)) {
   piggybank <- moneytotal
 }
 
-#lab step 3
+#lab step 3, creating a population change vector
 initialpopulation <- 2000
 rateofdecline <- .95
 for ( y in seq(1,7)) {
@@ -23,7 +23,7 @@ for ( y in seq(1,7)) {
   initialpopulation <- populationdecline
 }
 
-#lab step 4
+#lab step 4, creating a carrying capacity equation vector
 k <- 10000
 r <- 0.8
 n <- 2500
@@ -35,10 +35,10 @@ for ( t in seq(2,12)) {
 print(equation)
 
 #LAB PART 2
-#Lab step 1a
+#Lab step 1a, creating a vector of 0s
 rep(0, 18)
 
-#lab step 1b
+#lab step 1b, adding three to all integers in a vector
 for ( l in seq(1,18) ) {
   addingthrees <- rep(3 * (1:18))
 }
@@ -52,25 +52,25 @@ for ( l in seq(1,18) ) {
 }
 print(addingthreesagain)
 
-#lab step 1c
+#lab step 1c, altering a single point in a vector
 makeitone <- rep(0,18)
 makeitone[1] <- 1
 print(makeitone)
 
-#lab step 1d
+#lab step 1d, altering the string of numbers in a vector
 for ( p in seq(2,18)) {
   makeitone[p] <- (1 + (2 * makeitone[p-1]) )
 }
 print(makeitone)
 
-#lab step 2
+#lab step 2, creating a fibonacci sequence vector
 Fibonacci <- rep(1,20)
 for ( f in seq(3,20)) {
   Fibonacci[f] <- (Fibonacci[f-1] + Fibonacci[f-2])
 }
 print(Fibonacci)
 
-#lab step 3
+#lab step 3, creting a plot of carrying capacity data
 k <- 10000
 r <- 0.8
 n <- 2500
@@ -86,7 +86,21 @@ time <- seq(1,12)
 abundance <- equation
 plot(time, abundance, main = "Lab 04 Plot", xlab = "Time", ylab = "Abundance")
 
-#lab step 4
-help(setwd)
-setwd(~/Documents/CompBio/CompBioGithub/CompBioLabsandHomework/Lab04)
+#lab step 4a, accessing the csv
 getwd()
+setwd("/Users/rekna/Documents/CompBio/CompBioGithub/CompBioLabsAndHomework/Lab04")
+read.csv(file = "CO2_data_cut_paste.csv")
+matrix <- read.csv(file = "CO2_data_cut_paste.csv")
+nCol <- ncol(matrix)
+nRow <- nrow(matrix)
+
+#lab step 4b, calculating percent change in the matrix
+for ( q in 2:nCol ) {
+  for ( m in 2:nRow ) {
+    matrix[ m, q ] <- (matrix[m,q]/matrix[m-1,q])
+  }
+}
+print(matrix)
+
+#lab step 4c, writing the csv
+write.csv(x = matrix, file = "YearlyPercentChangesInCO2.csv")
