0e3ab64ad62683961ba39a044c6940686dff631a
JohnRosol <rosol.john@gmail.com>
2017-03-07 18:21:30 -0700

    finished table transformation to pairwise question on lab07

--- Lab07/Lab07.R
+++ Lab07/Lab07.R
@@ -19,7 +19,7 @@ LogGrowthFunction <- function( n = 10, Gens = 20, k = 1000 , r = .05) {
   plot(n, xlab = "Generations", ylab = "Abundance")
   return(n)
 }
-LogGrowthFunction()
+LogGrowthFunction(Gens = 200)
 
 
 
@@ -29,19 +29,37 @@ LogGrowthFunction()
   #relationships that occured in matrix 1 (row i and column j interacted = value 1)
 
 SimpleMatrix <- matrix( c(0,1,1,1,0,0,1,0,0), ncol = 3, nrow = 3)
+getwd()
+setwd("/Users/rekna/Documents/CompBio/CompBioGithub/CompBioLabsAndHomework/Lab07")
+Matrix <- read.csv("LargeAdjacencyMatrix.csv")
 
 TransformMatrixPairwise <- function( SqMatrix ) {
-  #making it a data fram will be easier, each collumn as object
-  SqMatrix( nrow = nRows, ncol = nCols )
-  for (i in 1:nCols) {
-    for (j in 1:nRows) {
-      if (SqMatrix [i,j] ! = 0) {
-        
-        #value = value, in if statement
+  nRows <- nrow(SqMatrix)
+  nCols <- ncol(SqMatrix)
+  value <- 1
+  zero <- 0
+  m <- 0
+  PairwiseCols <- 3
+  CurrentPairwise <- 1
+  for (q in 1:nRows) {
+    for (f in 1:nCols) {
+      if (SqMatrix[q,f] != zero) {
+        m <- ( m + 1 )
       }
     }
   }
+  PairwiseMatrix <- matrix( ncol = PairwiseCols, nrow = m)
+  for (j in 1:nRows) {
+    for (i in 1:nCols) {
+      if (SqMatrix[j,i] != zero) {
+        PairwiseMatrix[ CurrentPairwise , 1 ] <- j
+        PairwiseMatrix[ CurrentPairwise , 2 ] <- i
+        PairwiseMatrix[ CurrentPairwise , 3 ] <- value
+        CurrentPairwise <- ( CurrentPairwise + 1 )
+      }
+    }
+  }
+  return(PairwiseMatrix)
 }
 
-TransformMatrixPairwise( SqMatrix = SimpleMatrix)
-
+TransformMatrixPairwise( SqMatrix = Matrix )
