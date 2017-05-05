95b9d591921f98e5ab0f09c7f145423279f24758
JohnRosol <rosol.john@gmail.com>
2017-05-04 23:29:51 -0600

    Finalized lake parsing function to add row and column names to negative and significant value tables, built hard code to create tables for indicidual lake abundances

--- Project/ProjectRcode.R
+++ Project/ProjectRcode.R
@@ -1,4 +1,9 @@
 #Final Project
+
+#WRITE MARKDOWN FILE EXPLAINING PROCESS AND FINAL RESULTS
+#ANNOTATE THE SHIT OUT OF THIS BITCH
+
+
 #setting proper working directory
 getwd()
 setwd("/Users/rekna/Documents/CompBio/CompBioGithub/CompBioLabsAndHomework/Project")
@@ -8,16 +13,17 @@ library("psych")
 #setting all data sets to variables
 projectabundancedata <- read.csv(file.choose("Adamczyk_zooplankton_abundance_PLOSdata.csv"))
 
-
+P <- which( projectabundancedata[ , 1 ] == "Poway")
+Mi <- which( projectabundancedata[ , 1 ] == "Miramar")
+Mu <- which( projectabundancedata[ , 1 ] == "Murray")
 abundancerows <- nrow(projectabundancedata)
 wholeabundancedata <- as.matrix(projectabundancedata[ 1:abundancerows , 6:18])
-powaydata <- as.matrix(projectabundancedata[ 1:abundancerows , 6:18])
-miramardata <- as.matrix(projectabundancedata[ 1:abundancerows , 6:18])
-murraydata <- as.matrix(projectabundancedata[ 1:abundancerows , 6:18])
+powaydata <- as.matrix(projectabundancedata[ 1:length(P) , 6:18])
+miramardata <- as.matrix(projectabundancedata[ 1:length(Mi) , 6:18])
+murraydata <- as.matrix(projectabundancedata[ 1:length(Mu) , 6:18])
 
 
-for( r in 1:abundancerows) { #setting the abundance data to the total sample numbers by 
-#multiplying each species count by the "fraction of sample counted" collumn
+for( r in 1:abundancerows) { 
   wholeabundancedata[ r , 1 ] <- projectabundancedata[ r , 4 ] * projectabundancedata[ r , 6 ]
   wholeabundancedata[ r , 2 ] <- projectabundancedata[ r , 4 ] * projectabundancedata[ r , 7 ]
   wholeabundancedata[ r , 3 ] <- projectabundancedata[ r , 4 ] * projectabundancedata[ r , 8 ]
@@ -32,13 +38,33 @@ for( r in 1:abundancerows) { #setting the abundance data to the total sample num
   wholeabundancedata[ r , 12 ] <- projectabundancedata[ r , 4 ] * projectabundancedata[ r , 17 ]
   wholeabundancedata[ r , 13 ] <- projectabundancedata[ r , 4 ] * projectabundancedata[ r , 18 ]
 }
+#setting the abundance data to the total sample numbers by 
+#multiplying each species count by the "fraction of sample counted" collumn
 
-for( a in 1:abundancerows) { #need to figure out how to make lake charts
+myiterator <- 1
+for( a in 1:abundancerows) {
   if( projectabundancedata[ a , 1 ] == "Poway") {
-    powaydata[ a , 1 ] <- wholeabundancedata[ a , 1]
-      
+    powaydata[ myiterator , ] <- wholeabundancedata[ a , ]
+    myiterator <- myiterator + 1
   }
 }
+
+myiterator <- 1
+for( b in 1:abundancerows) {
+  if( projectabundancedata[ b , 1 ] == "Miramar") {
+    miramardata[ myiterator , ] <- wholeabundancedata[ b , ]
+    myiterator <- myiterator + 1
+  }
+}
+
+myiterator <- 1
+for( c in 1:abundancerows) {
+  if( projectabundancedata[ c , 1 ] == "Murray") {
+    murraydata[ myiterator , ] <- wholeabundancedata[ c , ]
+    myiterator <- myiterator + 1
+  }
+}
+
 # function for computing and printing all bacteria with competitive relationship
 competitionfinder <- function( lake ) {
   
@@ -48,8 +74,12 @@ competitionfinder <- function( lake ) {
   nRow <- nrow(rvalues)
   nCol <- ncol(rvalues)
   negativervalues <<- matrix(nrow = nRow, ncol = nCol)
+  rownames( negativervalues ) <<- rownames( rvalues )
+  colnames( negativervalues) <<- colnames( rvalues )
   pvalues <<- matrix(nrow = nRow, ncol = nCol)
-  significantvalues <<- matrix(nrow = nRow, ncol = nCol)
+  significantvalues <<- matrix(nrow = nRow, ncol = nCol) #must assign column values, 
+  
+  
   
   corrtest <- corr.test(lake)
   pvalues <<- round(corrtest$p, 8)
@@ -70,8 +100,11 @@ competitionfinder <- function( lake ) {
     }
   }
   
+  rownames( significantvalues ) <<- rownames( rvalues )
+  colnames( significantvalues) <<- colnames( rvalues )
+  
   for( q in 1:nRow){
-    for( t in 1:nCol){ #need to print values when both share an entry in the matrix
+    for( t in 1:nCol){ 
       if((!is.na(significantvalues[ q , t ])) 
           & 
           (!is.na(negativervalues[ q , t ]))) {
@@ -84,22 +117,6 @@ competitionfinder <- function( lake ) {
   
   if( mybool == 0)
   { print("No Competition")}
-  
   }
-competitionfinder( lake = wholeabundancedata)
+competitionfinder( lake = miramardata)
 
-rm(significancevalues)
-warnings()
-corr.test(wholeabundancedata)
-help(r.test)
-help(round)
-warnings()
-help("corr.test")
-help(print)
-?data
-
-
-
-
-#NAvalues <- which( is.na(negativervalues) )      
-#negativervalues[NAvalues] <<- 0  
